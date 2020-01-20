2b9504e6e65cc6145ac518d54a6785559b19ec37
PoBlue <250299430@qq.com>
2020-01-20 15:13:10 +0800

    fix active issues

--- js/app.js
+++ js/app.js
@@ -11,10 +11,7 @@ function elementInViewport2(el) {
     let bounding = el.getBoundingClientRect();
 
     return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        bounding.top <= (window.innerHeight || document.documentElement.clientHeight) / 2
     );
 }
 
