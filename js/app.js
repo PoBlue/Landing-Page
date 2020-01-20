62859ec31ed3a17a27b2b3fc3e9848876da18c6f
PoBlue <250299430@qq.com>
2020-01-20 11:00:06 +0800

    use getBoundingClientRect

--- js/app.js
+++ js/app.js
@@ -8,22 +8,13 @@ const sections = document.querySelectorAll("section");
 // check if an element in viewport 
 // reference: https://stackoverflow.com/a/125106
 function elementInViewport2(el) {
-    var top = el.offsetTop;
-    var left = el.offsetLeft;
-    var width = el.offsetWidth;
-    var height = el.offsetHeight;
-
-    while (el.offsetParent) {
-        el = el.offsetParent;
-        top += el.offsetTop;
-        left += el.offsetLeft;
-    }
+    let bounding = el.getBoundingClientRect();
 
     return (
-        top < (window.pageYOffset + window.innerHeight) &&
-        left < (window.pageXOffset + window.innerWidth) &&
-        (top + height) > window.pageYOffset &&
-        (left + width) > window.pageXOffset
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
 
